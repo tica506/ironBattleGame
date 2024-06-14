c0b8fad42cee397f9fe3cbba02b695330aa5ed6b
Alexia <ahamil20@fau.edu>
2024-06-14 18:13:47 -0400

    message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("bye world this is a test!");
+
+        int a = 17;
+        System.out.println("I am " + a + " years old");
     }
 }
