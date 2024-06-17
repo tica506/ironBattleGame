84c8a108d4bec3c7e0cb186baeeb1c1a7f76d7af
Tica506 <slentz3@wgu.edu>
2024-06-17 14:26:42 -0400

    today comment's -sofia

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("bye world this is a test!");
+        System.out.println("This is Sofia 06/17/2024");
         int a = 17;
         System.out.println("I am " + a + " years old");
     }
